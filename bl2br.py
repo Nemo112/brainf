0d930a0f2fb6d7fffac3522cb93773bbd7921351
nemo <martin.beranek112@gmail.com>
2014-04-16 13:37:58 +0200

    Přidán převodník mezi brainllolerem do brainfucku

--- bl2br.py
+++ bl2br.py
@@ -4,6 +4,74 @@ from optparse import OptionParser
 import os
 import zlib
 
+#Definice pro getchary a putchary
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+sys.stdout = Unbuffered(sys.stdout)
+
+class _Getch:
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+    def __call__(self): return self.impl()
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+getch = _Getch()
+
+def putchar(n):
+	print(chr(n), end='')
+
+def giveSymb(symbol):
+	if symbol==(255,0,0,255):
+		return(">")
+	elif symbol==(128,0,0,255):
+		return("<")
+	elif symbol==(0,255,0,255):
+		return("+")
+	elif symbol==(0,128,0,255):
+		return("-")
+	elif symbol==(0,0,255,255):
+		return(".")
+	elif symbol==(0,0,128,255):
+		return(",")
+	elif symbol==(255,255,0,255):
+		return("[")
+	elif symbol==(128,128,0,255):
+		return("]")
+	elif symbol==(0,255,255,255):
+		return("P")
+	elif symbol==(0,128,128,255):
+		return("L")
+	else:
+		return("X")
 def load_png_image(image):
     """Return width and height of PNG image
 
@@ -82,38 +150,100 @@ def isPng(inp):
 		return 0
 
 #Parsovani a nacitani
-parser = OptionParser(usage="Usage: %prog -f file", version="%prog 0.1")
-parser.add_option("-f","--FileName", action="store", dest="filename", type="string", help="Nastaveni jmena vstupniho souboru")
+parser = OptionParser(usage="usage: %prog [options] files", version="%prog 0.1")
+parser.add_option("-i","--FileInput", action="store", dest="filinp", type="string", help="Nastaveni jmena vstupniho souboru")
+parser.add_option("-o","--FileOutput", action="store", dest="filout", type="string", help="Nastaveni jmena vystupniho souboru")
 
 args = sys.argv[1:]
 (options, args) = parser.parse_args()
-if options.filename:
-	if not (os.path.exists(options.filename)):
+if options.filinp:
+	if not (os.path.exists(options.filinp)):
 		print("Soubor neexistuje")
-else:
-	print("Usage: inter_bl.by -f file.png")
-	exit(2);
+		exit(2)
+if options.filout == None :
+	print("Výstup není definován")
+	exit(2)
+if options.filinp == None :
+	print("Vstup není definován")
+	exit(2)
 
-i=0
-hd=b'';
-chunk=b'';
-pr=0
-with open(options.filename, 'rb') as f:
+matr=[]
+j=0
+maxx=0
+maxy=0
+with open(options.filinp, 'rb') as f:
 	image_properties = load_png_image(f)
 	pixels = load_png_data(f, image_properties)
+	#print(len(pixels))
 	for it in pixels:
+		matr.append([]);
+		maxy=len(it)
 		for i in it:
-			print(i)
-	print()
+			#print(giveSymb(i),end="")
+			matr[j].append(giveSymb(i));
+		#print(" r:" + str(j))
+		j+=1
+maxx=j
+#print(matr)
+class Patf:
+	x=0
+	y=0
+	v=0
+	def turnLeft(self):
+		self.v=(self.v-1)%4
+		self.step()
+	def turnRight(self):
+		self.v=(self.v+1)%4
+		self.step()
+	def step(self):
+		if self.v == 0:
+			self.x+=1
+		elif self.v == 1:
+			self.y+=1
+		elif self.v == 2:
+			self.x-=1
+		elif self.v == 3:
+			self.y-=1
+	def swVec(self):
+		if self.v == 0:
+			self.v=2
+		elif self.v == 1:
+			self.v=3
+		elif self.v == 2:
+			self.v=0
+		elif self.v==3:
+			self.v=1
+ch=Patf()
+bftxt=""
+# 0 ->
+# 1 dolu
+# 2 <-
+# 3 nahoru
+tape=[0]
+brstx=[]
+brsty=[]
+tapepnt=0
+o=1
 
-
-
-
-
-
-
-
-
-
+#print("maxy"+str(maxy)+"maxx"+str(maxx));
+x=ch.x
+y=ch.y
+outstr="";
+while maxx>y and maxy>x and y>=0 and x>=0:
+	if matr[y][x] == "P" :
+		ch.turnRight()
+	elif  matr[y][x] == "L" :
+		ch.turnLeft()
+	elif  matr[y][x] == "X" :
+		ch.step()
+	else:
+		outstr+=matr[y][x];
+		ch.step()
+	x=ch.x
+	y=ch.y
+print(outstr)
+fo = open(options.filout, "w")
+fo.write(outstr);
+fo.close()
 
 
